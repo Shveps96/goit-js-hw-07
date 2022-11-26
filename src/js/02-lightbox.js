52e0c84d81ee2b7f21d91768dc88daaee1f402cc
Shveps96 <42212039+Shveps96@users.noreply.github.com>
2022-11-26 18:10:06 +0200

    hw7

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,20 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>`
+    }).join('');
+}
+    let galleryLightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250,});
+    galleryLightbox.on('show.simplelightbox');
